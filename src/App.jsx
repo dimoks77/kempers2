01a35390a41960ec2e6bda343f885115e6b99ef6
Dmytro Kasko <dimoks77@gmail.com>
2024-08-15 15:13:10 +0200

    icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,51 @@
 import { useState, useEffect } from "react";
-import ContactForm from "./components/ContactForm/ContactForm";
-import ContactList from "./components/ContactList/ContactList";
-import SearchBox from "./components/SearchBox/SearchBox";
+// import ContactForm from "./components/ContactForm/ContactForm";
+// import ContactList from "./components/ContactList/ContactList";
+// import SearchBox from "./components/SearchBox/SearchBox";
+import Sidebar from "./components/Sidebar/Sidebar";
 
-const initialContacts = [
-  { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-  { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-  { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-  { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-];
+// const initialContacts = [
+//   { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+//   { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+//   { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+//   { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+// ];
 
 export const App = () => {
-  const [contacts, setContacts] = useState(() => {
-    const storedContacts = JSON.parse(localStorage.getItem('storedContacts'));
-    return storedContacts || initialContacts;
-  });
+  // const [contacts, setContacts] = useState(() => {
+  //   const storedContacts = JSON.parse(localStorage.getItem('storedContacts'));
+  //   return storedContacts || initialContacts;
+  // });
 
-  useEffect(() => {
-    localStorage.setItem('storedContacts', JSON.stringify(contacts));
-  }, [contacts]);
+  // useEffect(() => {
+  //   localStorage.setItem('storedContacts', JSON.stringify(contacts));
+  // }, [contacts]);
 
-  const [nameFilter, setNameFilter] = useState('');
+  // const [nameFilter, setNameFilter] = useState('');
 
-  const addContact = (newContact) => {
-    setContacts((prevContacts) => {
-      return [...prevContacts, newContact];
-    });
-  };
+  // const addContact = (newContact) => {
+  //   setContacts((prevContacts) => {
+  //     return [...prevContacts, newContact];
+  //   });
+  // };
 
-  const deleteContact = (contactId) => {
-    setContacts((prvContacts) => {
-      return prvContacts.filter((contact) => contact.id !== contactId);
-    });
-  };
+  // const deleteContact = (contactId) => {
+  //   setContacts((prvContacts) => {
+  //     return prvContacts.filter((contact) => contact.id !== contactId);
+  //   });
+  // };
 
-  const visibleContacts = contacts.filter((contact) =>
-    contact.name.toLowerCase().includes(nameFilter.toLowerCase())
-  );
+  // const visibleContacts = contacts.filter((contact) =>
+  //   contact.name.toLowerCase().includes(nameFilter.toLowerCase())
+  // );
 
   return (
     <>
-      <h1>PhoneBook</h1>
-      <ContactForm onAdd={addContact} />
-      <SearchBox value={nameFilter} onChange={setNameFilter} />
-      <ContactList items={visibleContacts} onDelete={deleteContact} />
+      <h1>test</h1>
+      <Sidebar />
+      {/* <ContactForm onAdd={addContact} /> */}
+      {/* <SearchBox value={nameFilter} onChange={setNameFilter} /> */}
+      {/* <ContactList items={visibleContacts} onDelete={deleteContact} /> */}
     </>
   );
 };
---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,12 @@
+import css from "./Sidebar.module.css";
+
+const Sidebar = () => {
+  return (
+    <div className={css.search}>
+      <p>еуіе</p>
+      {/* <input className={css.input} type="text" value={value} onChange={(evt) => onChange(evt.target.value)} /> */}
+    </div>
+  );
+};
+
+export default Sidebar;
